336d45840d0bc1f22f7af56470486f2b8e9a8f71
Shafujiu <981215707@qq.com>
2020-07-29 22:43:16 +0800

    Initial Commit

---
+++ SFJNameSpaceDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SFJNameSpaceDemo
+//
+//  Created by Shafujiu on 2020/7/29.
+//  Copyright © 2020 Shafujiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
